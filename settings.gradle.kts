13e1acf6e51c7b26231190ec3eec9fba35bb8903
Merc98 <148023831+Merc98@users.noreply.github.com>
2026-04-29 20:36:31 -0400

    Remove manual Android plugin resolution hack

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.android.application" || requested.id.id == "com.android.library") {
-                useModule("com.android.tools.build:gradle:${requested.version}")
-            }
-        }
-    }
 }
 
 dependencyResolutionManagement {
